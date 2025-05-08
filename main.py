399ab57c2d2d90d5d8a216ac8d5d5a76f8bf383e
zaluce <145462429+luceurbae@users.noreply.github.com>
2025-05-08 16:45:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ root.title("Auto Clicker GUI")
 
 tk.Label(root, text="Auto Clicker by Zaluce", font=("Arial", 14)).pack(pady=10)
 
-tk.Button(root, text="Rekam Klik", width=25, command=start_recording).pack(pady=5)
-tk.Button(root, text="Mainkan Klik (Loop)", width=25, command=start_playing).pack(pady=5)
-tk.Button(root, text="Hentikan Pemutaran", width=25, command=stop_playing, fg="red").pack(pady=5)
+tk.Button(root, text="Record Click", width=25, command=start_recording).pack(pady=5)
+tk.Button(root, text="Play Click (Loop)", width=25, command=start_playing).pack(pady=5)
+tk.Button(root, text="Stop", width=25, command=stop_playing, fg="red").pack(pady=5)
 
-tk.Label(root, text="Klik kanan untuk hentikan perekaman.\nKlik 'Hentikan Pemutaran' untuk stop loop.", font=("Arial", 9)).pack(pady=10)
+tk.Label(root, text="Right-click to stop recording.\nClick 'Stop' for stop looping.", font=("Arial", 9)).pack(pady=10)
 
 root.mainloop()
